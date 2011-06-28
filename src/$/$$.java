b6865477227df76c57862efb8fb41895644146ad
Adam Lehenbauer <adam.lehenbauer@gmail.com>
2011-06-28 18:19:27 -0400

    adds $ package

---
+++ src/$/$$.java
@@ -0,0 +1,9 @@
+package $;
+
+public class $$ {
+	public static class $$$ {
+		public static void main(String[] args) {
+			System.out.println("hello from class " + $$$.class.getName());
+		}
+	}
+}
---
+++ src/$/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Have fun escaping this on the command line.
+ * If your shell prompt is $ it makes for some interesting command lines.
+ */
